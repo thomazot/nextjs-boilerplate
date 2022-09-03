2be815d89aadbd5e1a5de9a6f3988ffc5e1af4e3
Thomaz Toyama <contato@thomazot.com.br>
2022-09-02 22:07:37 -0300

    chore: update version with ncp

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: isProd
   }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd,
+  register: true
 })
+
+module.exports = withPWA(nextConfig)
