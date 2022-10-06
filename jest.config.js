cb134745c3e5368ead7de614392ee2ad3d719fbc
Thomaz Toyama <contato@thomazot.com.br>
2022-10-06 08:55:47 -0300

    chore: ignore folders styles, stores and types

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,13 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.stories.tsx',
+    '!src/styles/**/*',
+    '!src/stores/**/*',
+    '!src/types/**/*'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/']
 }
